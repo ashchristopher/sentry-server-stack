d1c47becd9afc8ac44bd103c070e6abfd8f33588
Ash Christopher <ash.christopher@gmail.com>
2010-10-18 16:10:46 -0400

    Added static

--- sentry_server/urls.py
+++ sentry_server/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.conf import settings
 
 
 admin.autodiscover()
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^sentry/', include('sentry.urls')),
+    url(r'^media-server/(?P<path>.*)$', 'django.views.static.serve', { 'document_root' : settings.MEDIA_ROOT }, name='media'),
 )
